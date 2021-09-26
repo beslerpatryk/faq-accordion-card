a2bf523aba5c6bc32d7d00af39012a21f6846ecb
beslerpatryk <beslerpatryk@gmail.com>
2021-09-26 23:28:33 +0800

    feat: improve accordion functionality

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 const buttonList = document.querySelectorAll(".btn");
 const answerList = document.querySelectorAll(".answer");
 const questionList = document.querySelectorAll(".question");
+const questionWrapperEl = document.querySelectorAll(".question_wrapper");
 const containerList = document.getElementsByTagName("li");
 const ulEl = document.getElementsByTagName("ul");
+const card = document.getElementById("card");
 
-function toggleUnfold(buttons, questions, answers, containers, list) {
-	buttons.forEach((button, idx) => {
-		button.addEventListener("click", () => {
-			if (answers[idx].style.display === "none") {
-				console.log("unfolding");
-				answers[idx].style.display = "block";
-				questions[idx].style.fontWeight = "700";
-				let answerHeight = answers[idx].offsetHeight;
-				ulEl[0].style.transform = `translateY(${answerHeight}px)`;
-				// containers[idx].style.marginBottom = "50px";
-			} else {
-				console.log("folding");
-				answers[idx].style.display = "none";
-				questions[idx].style.fontWeight = null;
-				ulEl[0].style.transform = null;
-
-				// containers[idx].style.marginBottom = null;
-			}
+function toggleUnfold(buttons, questions, answers, questionWrapperEl) {
+	questionWrapperEl.forEach((wrapper, idx) => {
+		wrapper.addEventListener("click", () => {
+			wrapper.classList.toggle("unfold");
+			buttons[idx].classList.toggle("unfold");
+			answers[idx].classList.toggle("unfold");
+			questions[idx].classList.toggle("unfold");
 		});
 	});
 }
 
-toggleUnfold(buttonList, questionList, answerList, containerList, ulEl);
+toggleUnfold(buttonList, questionList, answerList, questionWrapperEl);
